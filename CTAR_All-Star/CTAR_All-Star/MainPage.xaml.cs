263ffced9bd083155eb6b845fcc22ea219535463
Terri.work.17 <Terri.work.17@gmail.com>
2018-10-16 16:22:43 -0700

    Add project files.

---
+++ CTAR_All-Star/CTAR_All-Star/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CTAR_All_Star
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
