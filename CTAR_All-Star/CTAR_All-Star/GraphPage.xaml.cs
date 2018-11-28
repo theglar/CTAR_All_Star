0cf3209dc9f2269d04e4e2a17abc1d6da6eb12d2
Terri.work.17 <Terri.work.17@gmail.com>
2018-11-27 20:36:58 -0800

    Working simulation but very choppy

--- CTAR_All-Star/CTAR_All-Star/GraphPage.xaml.cs
+++ CTAR_All-Star/CTAR_All-Star/GraphPage.xaml.cs
@@ -2,23 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Microcharts.Forms;
 using SkiaSharp;
 using Microcharts;
+using CTAR_All_Star.Model;
 
 using Syncfusion.SfChart.XForms;
 
 namespace CTAR_All_Star
-{        
+{
     public partial class GraphPage : ContentPage
     {        
         public GraphPage()
         {
             InitializeComponent();            
+        }
+       
+        private void Start_Simulation(object sender, EventArgs e)
+        {           
+            // Initialize a starting point
+            Double pressure = 0;
+
+            //Loop 100 times
+            for (int i = 0; i < 100; i++)
+            {
+                //Create and add a measurement to the database
+                // Get current date and time
+                DateTime d = DateTime.Now;
+                DateTime dt = DateTime.Parse(d.ToString());
+
+                //Top threshold - start going down
+                pressure = Math.Sin(Convert.ToDouble(d.Millisecond)/10);
+
+                Measurement measurement = new Measurement()
+                {
+                    UserName = "Tester 1",
+                    SessionNumber = "1",
+                    TimeStamp = d,
+                    Pressure = pressure,
+                    Duration = "1",
+                    DisplayTime = dt.ToString("HH:mm:ss")
+                };
+
+                using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+                {
+                    conn.CreateTable<Measurement>();
+                    conn.Insert(measurement);
+                }
+
+                //Wait 0.25 seconds
+                //Thread.Sleep(250);
+
+                //Refresh page
+                Navigation.PushAsync(new GraphPage());
+            }
         }
     }
 }
--- CTAR_All-Star/CTAR_All-Star/Model/ViewModel.cs
+++ CTAR_All-Star/CTAR_All-Star/Model/ViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using Xamarin.Forms;
 using CTAR_All_Star;
@@ -10,21 +11,24 @@ namespace CTAR_All_Star.Model
 {
     public class ViewModel  
     { 
-      public List<Measurement> Data { get; set; }
+      public ObservableCollection<Measurement> Data { get; set; }
 
       public ViewModel()       
       {
-            Data = new List<Measurement>();
+            Data = new ObservableCollection<Measurement>();
 
             // Connect to database, pull data and store it in the list
             using (SQLiteConnection conn = new SQLiteConnection(App.DB_PATH))
             {
                 //To display all data in the table
                 var table = conn.Table<Measurement>();
+                table = table.OrderByDescending(x => x.Id).Take(10);
+                table = table.OrderBy(x => x.Id);
                 foreach (var m in table)
                 {
-                    Data.Add(m);               
+                    Data.Add(m);
                 }
+                
             }
         }
     }
