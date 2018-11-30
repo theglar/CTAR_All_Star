84b5980d29011b45b638a0871f6b089a99d0aac3
Andrew Penrose <apenrose@nevada.unr.edu>
2018-11-29 18:28:08 -0800

    Confirm Pass added to signup

--- CTAR_All-Star/CTAR_All-Star/Views/SignupPage.xaml.cs
+++ CTAR_All-Star/CTAR_All-Star/Views/SignupPage.xaml.cs
@@ -20,11 +20,13 @@ namespace CTAR_All_Star.Views
             Lbl_DocID.TextColor = Constants.MainTextColor;
             Lbl_EMR.TextColor = Constants.MainTextColor;
             Lbl_NewPassword.TextColor = Constants.MainTextColor;
+            Lbl_ConfirmPass.TextColor = Constants.MainTextColor;
             ActivitySpinner.IsVisible = false;
 
             Entry_DocID.Completed += (s, e) => Entry_NewPassword.Focus();
             Entry_EMR.Completed += (s, e) => Entry_NewPassword.Focus();
-            Entry_NewPassword.Completed += (s, e) => SignUpProcedure(s, e);
+            Entry_NewPassword.Completed += (s, e) => Entry_ConfirmPass.Focus();
+            Entry_ConfirmPass.Completed += (s, e) => SignUpProcedure(s, e);
         }
 
         void SignUpProcedure(object sender, EventArgs e)
