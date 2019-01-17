792b71eddd301de5ee9338b236e8444d3a52de2c
Terri.work.17 <Terri.work.17@gmail.com>
2019-01-16 18:33:30 -0800

    yep

--- CTAR_All-Star/CTAR_All-Star.Android/MainActivity.cs
+++ CTAR_All-Star/CTAR_All-Star.Android/MainActivity.cs
@@ -9,6 +9,12 @@ using Android.OS;
 using System.IO;
 using CTAR_All_Star;
 using Plugin.Permissions;
+using Android.Support.V4.Content;
+using Android;
+using Android.Support.V4.App;
+using Android.Util;
+using Android.Support.Design.Widget;
+using System.Threading.Tasks;
 
 namespace CTAR_All_Star.Droid
 {
@@ -28,7 +34,70 @@ namespace CTAR_All_Star.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(full_path));
+
+           
+
+            //if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation) == (int)Permission.Granted
+            //    && ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) == (int)Permission.Granted)
+            //{
+            //    // We have permission, go ahead and use the location.
+            //}
+            //else
+            //{
+            //    // Location permission is not granted. If necessary display rationale & request.
+            //}
         }
+
+        async Task TryToGetPermissions()
+        {
+            if((int)Build.VERSION.SdkInt >= 23)
+            {
+                await GetPermissionsAsync();
+                return;
+            }
+        }
+
+        const int RequestLocationId = 0;
+
+        readonly string[] PermissionsGroupLocation =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+        };
+
+        async Task GetPermissionsAsync()
+        {
+            const string permission = Manifest.Permission.AccessFineLocation;
+
+            if (CheckSelfPermission(permission) == (int)Android.Content.PM.Permission.Granted)
+            {
+                Toast.MakeText(this, "Permission Granted", ToastLength.Short).Show();
+                return;
+            }
+
+            if(ShouldShowRequestPermissionRationale(permission))
+            {
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.SetTitle("Permissions Needed");
+                alert.SetMessage("CTAR All-Star needs permission to continue");
+                alert.SetPositiveButton("Request Permissions", (senderAlert, args) =>
+                {
+                    RequestPermissions(PermissionsGroupLocation, RequestLocationId);
+                });
+                alert.SetNegativeButton("Cancel", (senderAlert, args) =>
+                {
+                    Toast.MakeText(this, "Cancelled", ToastLength.Short);
+                });
+
+                Dialog dialog = alert.Create();
+                dialog.Show();
+
+                return;
+            }
+        }
+
+        //https://www.youtube.com/watch?v=Uzpy3qdYXmE
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
